abe782b113084393717e926011fb7077cc1e1d8b
Setyabudi90 <dbgaming679@gmail.com>
2024-09-24 15:55:51 +0700

    commit

---
+++ script.js
@@ -0,0 +1,101 @@
+const hero = document.querySelector(".hero"),
+  header = document.querySelector("header"),
+  menu = document.querySelector("#menu"),
+  nav = document.querySelector("header nav ul");
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       header.classList.add("glass");
+//       console.log(entry);
+//     } else {
+//       header.classList.remove("glass");
+//     }
+//   });
+// });
+
+// observer.observe(hero);
+
+window.onscroll = () => {
+  header.classList.toggle("glass", window.scrollY > 0);
+};
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  if (!header.classList.contains("glass")) {
+    header.classList.toggle("glass");
+  }
+  if (nav.classList.contains("active")) {
+    menu.classList.replace("fa-bars", "fa-times");
+  } else {
+    menu.classList.replace("fa-times", "fa-bars");
+  }
+});
+
+window.addEventListener("click", function (event) {
+  if (
+    nav.classList.contains("active") &&
+    event.target != nav &&
+    event.target != menu
+  ) {
+    nav.classList.remove("active");
+    header.classList.remove("glass");
+    menu.classList.replace("fa-times", "fa-bars");
+  }
+});
+
+document.querySelector("#search-icon").onclick = () => {
+  document.querySelector("#search-form").classList.toggle("active");
+};
+
+document.querySelector("#close").onclick = () => {
+  document.querySelector("#search-form").classList.remove("active");
+};
+
+const roles = [
+  "Web Developer",
+  "Software Engineer",
+  "Backend Developer",
+  "Frontend Developer",
+  "Fullstack Developer",
+  "React Developer",
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+const typingSpeed = 100;
+const delayAfterTyping = 4000;
+
+function changeRole() {
+  const roleElement = document.getElementById("role");
+  const currentRole = roles[roleIndex];
+
+  roleElement.textContent = "";
+  roleElement.style.width = "0ch";
+  charIndex = 0;
+
+  function change() {
+    if (charIndex < currentRole.length) {
+      roleElement.textContent += currentRole.charAt(charIndex);
+
+      roleElement.style.width = `${charIndex}ch`;
+
+      charIndex++;
+
+      setTimeout(change, typingSpeed);
+    } else {
+      setTimeout(() => {
+        roleIndex = (roleIndex + 1) % roles.length;
+        changeRole();
+      }, delayAfterTyping);
+    }
+  }
+
+  change();
+}
+
+changeRole();
+
+document.querySelector(
+  "footer .copyright"
+).innerHTML = `&copy; ${new Date().getFullYear()} Made with <i class="fa-solid fa-heart" style="color: red;"></i> By <strong style="color: var(--senary-color);">Inggrit Setya Budi</strong>`;
